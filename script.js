b5e57b74e4b0f90eddd8a159e14a38e4bbc07017
ulugbekbrat <168886327+ulugbekbrat@users.noreply.github.com>
2024-08-15 10:26:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let a = prompt('siz typedagi malumot kiriting')
+console.log(typeof a);
+
+
+let b = prompt('number typedagi malumot kiriting ')
+console.log(typeof b);
+
+
+let c = prompt('boolean typedagi malumot kiriting')
+console.log(typeof c);
+
+let d = confirm('hamma malumotlarni togri kiritganingizga ishonchingiz ')
+
+console.log(d);
+
+
+alert('siz kiritgan soz string typega tegishli'  )
+alert('siz kiritgan soz nuber typega tegishli'  )
+alert('siz kiritgan soz boolean typega tegishli'  )
